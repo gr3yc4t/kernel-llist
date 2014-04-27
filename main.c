40083ba789d0c02c135c5c0e822581fdf50aaa4c
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2014-04-27 13:47:15 +0100

    Remove warning for argc and argv.

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "animal_list.h"
 
 int main(int argc, char *argv[])
 {
+    if (argc != 1)
+    {
+        printf("USAGE: %s\n", argv[0]);
+        return -1;
+    }
 
     return 0;
 }
